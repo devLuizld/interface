5f1817a2a2a5aa11e2556102ee4f913be6130f83
devLuizld <luizld1212@gmail.com>
2024-08-16 21:40:53 -0300

    Update interface.py

--- interface/interface.py
+++ interface/interface.py
@@ -27,35 +27,35 @@ def calcular_imc():
     except ValueError:
         tk.messagebox.showerror("Erro", "Por favor, insira valores numéricos válidos.")
 
-# Configuração da janela principal
+
 root = tk.Tk()
 root.title("Calculadora de IMC")
 root.geometry("300x200")
 
-# Altera a cor de fundo da janela principal
-root.configure(bg='lightblue')  # Define a cor de fundo para 'lightblue'
 
-# Configuração do campo de entrada de peso
-label_peso = tk.Label(root, text="Peso (kg):", bg='lightblue')  # Define a cor de fundo do label
+root.configure(bg='lightblue')  
+
+
+label_peso = tk.Label(root, text="Peso (kg):", bg='lightblue')  
 label_peso.pack(pady=5)
 
 entrada_peso = tk.Entry(root)
 entrada_peso.pack(pady=5)
 
-# Configuração do campo de entrada de altura
-label_altura = tk.Label(root, text="Altura (cm):", bg='lightblue')  # Define a cor de fundo do label
+
+label_altura = tk.Label(root, text="Altura (cm):", bg='lightblue')  
 label_altura.pack(pady=5)
 
 entrada_altura = tk.Entry(root)
 entrada_altura.pack(pady=5)
 
-# Botão para calcular o IMC
+
 botao_calcular = tk.Button(root, text="Calcular IMC", command=calcular_imc)
 botao_calcular.pack(pady=10)
 
-# Label para exibir o resultado
-label_resultado = tk.Label(root, text="", bg='lightblue')  # Define a cor de fundo do label
+
+label_resultado = tk.Label(root, text="", bg='lightblue')  
 label_resultado.pack(pady=10)
 
-# Inicia o loop principal do Tkinter
+
 root.mainloop()
